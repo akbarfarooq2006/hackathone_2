bd1f04059ea88e7831446a3b542bd0a915a61e39
akbar farooq <akbarfarooq2006@gmail.com>
2024-12-08 13:56:54 +0500

    Aap ka commit message

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint warnings/errors during Vercel builds
+  },
+  /* Add other Next.js configurations here */
 };
 
 export default nextConfig;
